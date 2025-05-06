f86fd3b24ff8e087b1fce6f9249ed614b8047bb3
Samuel <samuelbelanddeltil@hotmail.com>
2025-05-06 12:43:14 -0700

    Add views and update server.js, ignore public folder

--- server.js
+++ server.js
@@ -1,12 +1,54 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+
+// Route to render your EJS file
 app.get('/', (req, res) => {
-  res.send('Hello from Bachir Project!');
+  res.render('home-page')
+});
+
+app.get('/gallery', (req, res) => {
+    res.render('gallery')
+}); 
+
+//links to different collection of painting
+app.get('/black-collection', (req, res) =>  {
+    res.render('gallery/black-collection')
+});
+
+app.get('/mor-talla-collection', (req, res) =>  {
+    res.render('gallery/mor-talla')
+});
+
+app.get('/family', (req, res) =>    {
+    res.render('gallery/family')
+});
+
+app.get('/special-edition', (req, res) =>   {
+    res.render('gallery/special-edition')
+});
+
+app.get('/exhibitions', (req, res) =>   {
+    res.render('gallery/exhibitions')
+});
+
+app.get('/about', (req, res) => {
+    res.render('about')
+});
+
+app.get('/store', (req, res) => {
+    res.render('store')
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
