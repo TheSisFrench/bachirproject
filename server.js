3e703b2a70c911912e4dfe5b516bb018e6d63ef5
Samuel <samuelbelanddeltil@hotmail.com>
2025-05-08 21:53:48 -0700

    setting views accessible using path join

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const { title } = require('process');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'));
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
@@ -18,45 +19,53 @@ app.get('/', (req, res) => {
   res.render('index', {
     currentPage: 'home',
     title: 'home',
-    bodyPartialName: 'partials/home'
+    bodyPartialName: 'home'
   });
 });
 
 app.get('/gallery', (req, res) => {
-    res.render('gallery')
+    res.render('index',  {
+        currentPage: 'gallery',
+        title: 'gallery',
+        bodyPartialName: 'gallery'
+    });
 }); 
 
 //links to different collection of painting
-app.get('/black-collection', (req, res) =>  {
-    res.render('gallery/black-collection')
+app.get('/gallery/black-collection', (req, res) =>  {
+    res.render('index', {
+        currentPage: 'mor talla',
+        title: 'Mor talla',
+        bodyPartialName: 'mor-talla'
+    })
 });
 
-app.get('/mor-talla-collection', (req, res) =>  {
-    res.render('gallery/mor-talla')
+app.get('/gallery/mor-talla-collection', (req, res) =>  {
+    res.render('index')
 });
 
-app.get('/family', (req, res) =>    {
-    res.render('gallery/family')
+app.get('/gallery/family', (req, res) =>    {
+    res.render('index')
 });
 
-app.get('/special-edition', (req, res) =>   {
-    res.render('gallery/special-edition')
+app.get('/gallery/special-edition', (req, res) =>   {
+    res.render('index')
 });
 
 app.get('/exhibitions', (req, res) =>   {
-    res.render('exhibitions')
+    res.render('index')
 });
 
 app.get('/about', (req, res) => {
-    res.render('about')
+    res.render('index')
 });
 
 app.get('/contact', (req, res) =>   {
-    res.render('contact')
+    res.render('index')
 });
 
 app.get('/store', (req, res) => {
-    res.render('store')
+    res.render('index')
 });
 
 app.listen(PORT, () => {
